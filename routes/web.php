07c62494d4a381e7afb4e807b4d966a189091917
honorine piolat <148060897+honopio@users.noreply.github.com>
2024-02-24 18:20:22 +0000

    added controllers, views and database

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,14 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+use App\Http\Controllers\HomeController;
+    Route::get('/', [HomeController::class, 'index']);
+use App\Http\Controllers\ContactController;
+    Route::get('/contact', [ContactController::class, 'index']);
+use App\Http\Controllers\RecetteController;
+    Route::get('/contact', [RecetteController::class, 'index']);
