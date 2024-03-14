e4b0f6dc7d92e50378e4e1216373c224e134df26
honopio <honorine.piolat@gmail.com>
2024-03-14 16:11:41 +0100

    new homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,29 @@
 @extends('layouts/main')
 @section('content')
-    <h1>Home content</h1>
+    <h1 class="mt-2 mb-2 is-size-3 is-size-4-mobile has-text-weight-bold">Les 3 dernières recettes : </h1>
+    <br>
 
+
+    @if ( $recipes->isEmpty() )
+    <p>No recipes found</p>
+
+    @else
+        <div class="columns is-multiline">
+            {{-- for the first 3 recipes : --}}
+            @foreach ( $recipes->take(3) as $recipe )
+
+
+            <div class="column is-4 mb-5">
+              <span><small class="has-text-grey-dark">{{ $recipe->updated_at }}</small></span>
+              <h2 class="mt-2 mb-2 is-size-3 is-size-4-mobile has-text-weight-bold">{{ $recipe->title }}</h2>
+              <p class="subtitle has-text-grey">{{ $recipe->content }}</p>
+              <a href="#">Read More</a>
+            </div>
+    @endforeach
+</div>
+
+
+    @endif
           {{-- <div class="columns is-multiline">
             <div class="column is-4 mb-5">
               <span><small class="has-text-grey-dark">10 jun 2021 19:40</small></span>
@@ -22,18 +44,5 @@
               <a href="#">Read More</a>
             </div>
           </div> --}}
-
-          @if ( $recipes->isEmpty() )
-              <p>No recipes found</p>
-
-          @else
-
-          @endif
-
-          <ul>
-            @foreach ( $recipes as $recipe )
-             <li>{{ $recipe->title }}</li>
-            @endforeach
-          </ul>
 
 @endsection
