4c022eecda5f1225b4e7e858a31636d93dc43368
honopio <honorine.piolat@gmail.com>
2024-03-14 15:28:00 +0100

    faker done

--- database/factories/RecipeFactory.php
+++ database/factories/RecipeFactory.php
@@ -3,7 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-
+use App\Models\User;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Recipe>
  */
@@ -16,8 +16,18 @@ class RecipeFactory extends Factory
      */
     public function definition(): array
     {
+        $this->faker->addProvider(new \FakerRestaurant\Provider\fr_FR\Restaurant($this->faker));
+        $title = $this->faker->numerify($this->faker->foodName(). ' ###');
         return [
-            //
+         'user_id' => User::inRandomOrder()->first()->id, // Get a random user ID,
+         'title' => $title,
+         'content' => $this->faker->paragraph($nbSentences = 10, $variableNbSentences = true),
+         'ingredients' => $this->faker->vegetableName().", ".$this->faker->meatName(),
+         'price' => $this->faker->words($nb = 1, $asText = true),
+         'url' => str_replace(' ', '-', $title),
+         //50% chance of the recipe having tags. if so, between 1 and 5 tags are generated
+         'tags' => $this->faker->boolean(50) ? $this->faker->words($nb = $this->faker->numberBetween(1, 5), $asText = true) : null,
+         'status' => 'published',
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        //creation de 10 users aleatoires
         \App\Models\User::factory(10)->create();
 
-        \App\Models\User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        //creation d'un user unique
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        \App\Models\Recipe::factory()
+            ->count(15)
+            ->create()
+
+            //random user from the users table for each recipe
+            ->each(function ($recipe) {
+                $recipe->user_id = \App\Models\User::inRandomOrder()->first()->id;
+                $recipe->save();
+            });
     }
 }
