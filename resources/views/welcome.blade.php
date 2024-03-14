7129b1b9a37bb48ff67546fc300b46e89c95c430
honopio <honorine.piolat@gmail.com>
2024-03-14 15:59:01 +0100

    views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <h1>Home content</h1>
 
-          <div class="columns is-multiline">
+          {{-- <div class="columns is-multiline">
             <div class="column is-4 mb-5">
               <span><small class="has-text-grey-dark">10 jun 2021 19:40</small></span>
               <h2 class="mt-2 mb-2 is-size-3 is-size-4-mobile has-text-weight-bold">Lorem ipsum dolor sit</h2>
@@ -21,7 +21,14 @@
               <p class="subtitle has-text-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque massa nibh, pulvinar vitae aliquet nec, accumsan aliquet orci.</p>
               <a href="#">Read More</a>
             </div>
-          </div>
+          </div> --}}
+
+          @if ( $recipes->isEmpty() )
+              <p>No recipes found</p>
+
+          @else
+
+          @endif
 
           <ul>
             @foreach ( $recipes as $recipe )
