177914c6a5bef6112b199337a26b429898170bbc
honopio <honorine.piolat@gmail.com>
2024-03-20 13:11:02 +0100

    add sauvegade contact en DB

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Contact;
 
 class ContactController extends Controller
 {
@@ -10,12 +11,21 @@ class ContactController extends Controller
         return view('contact');
     }
     //ajout de la fonction store pour le formulaire de contact
-    function store(Request $request){
+    /*function store(Request $request){
         $request->validate([
             'nom' => 'required',
             'email' => 'required|email',
             'message' => 'required'
         ]);
         return back()->with('message', 'Votre message a bien été envoyé.');
-    }
+    }*/
+
+    function store() {
+        $contact = new Contact();
+      //  $contact->nom = request('nom');
+        $contact->email = request('email');
+        $contact->message = request('message');
+        $contact->save();
+        return back()->with('message', 'Votre message a bien été envoyé.');
+}
 }
